a72683a72030ef59f9a1638d4182bae4024ac9d7
dn1984 <kils@list.ru>
2021-06-04 01:08:12 +0400

    Загружены картинки для ретина дисплеев, исправлен скрипт мобильного меню закрывающий меню при ресайзе окна более 768

--- src/pages/main/mobile-menu.js
+++ src/pages/main/mobile-menu.js
@@ -1,29 +1,26 @@
 let mobileMenuOpen = document.querySelector("#mobile-btn-open"),
     mobileMenuClose = document.querySelector("#mobile-btn-close"),
     mobilemenu = document.querySelector(".header-mobile");
-    
-//mobile menu
 
 mobileMenuOpen.addEventListener ('click', () => {
-
     if(mobilemenu.classList.contains("header-mobile-hidden")) {
-      
 		mobilemenu.classList.remove('header-mobile-hidden');
-      
 	}
-    // overlay.classList.add('popup-visible');
-    // modalWindow.classList.add('popup-visible');
-    // modalInput.focus();
 })
 
 mobileMenuClose.addEventListener ('click', () => {
-
     if(!mobilemenu.classList.contains("header-mobile-hidden")) {
-      
 		mobilemenu.classList.add('header-mobile-hidden');
-      
 	}
-    // overlay.classList.add('popup-visible');
-    // modalWindow.classList.add('popup-visible');
-    // modalInput.focus();
 })
+
+document.addEventListener("DOMContentLoaded", function(event)
+{
+    window.onresize = function() {
+        if (window.outerWidth>768) {
+            if(!mobilemenu.classList.contains("header-mobile-hidden")) {
+                mobilemenu.classList.add('header-mobile-hidden');
+            }
+        }
+    };
+});
