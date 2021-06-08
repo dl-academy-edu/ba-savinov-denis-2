7f6c4555ba51f6e18bd59e539f83095c684e0857
Denis Savinov <kils@list.ru>
2021-06-08 15:04:49 +0400

    Исправлен адаптив слайдера swiper

--- src/pages/main/swiper-slider.js
+++ src/pages/main/swiper-slider.js
@@ -1,49 +1,91 @@
-console.log (window.screen.width);
+// if (window.screen.width > 1200) {
+//     sliderSwiper(4, 20);
+// } else if (window.screen.width < 1200 && window.screen.width > 770) {
+//     sliderSwiper(3, 10);
+// } else if (window.screen.width <= 770 && window.screen.width > 766) {
+//     sliderSwiper(2.618, 19.78);
+// } else if (window.screen.width <= 766 && window.screen.width > 450) {
+//     sliderSwiper(2, 10);
+// } else if (window.screen.width <= 450 && window.screen.width > 376) {
+//     sliderSwiper(1.5, 10);
+// } else if (window.screen.width <= 376) {
+//     sliderSwiper(1.33, 16);
+// }
 
-if (window.screen.width > 1200) {
-    sliderSwiper(4, 20);
-} else if (window.screen.width < 1200 && window.screen.width > 770) {
-    sliderSwiper(3, 10);
-} else if (window.screen.width <= 770 && window.screen.width > 766) {
-    sliderSwiper(2.618, 19.78);
-} else if (window.screen.width <= 766 && window.screen.width > 450) {
-    sliderSwiper(2, 10);
-} else if (window.screen.width <= 450 && window.screen.width > 376) {
-    sliderSwiper(1.5, 10);
-} else if (window.screen.width <= 376) {
-    sliderSwiper(1.33, 16);
-}
 
-console.log(window.screen.width);
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    cssMode: false,
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    window.onresize = function() {
-        if (window.outerWidth > 1200) {
-            sliderSwiper(4, 20);
-        } else if (window.outerWidth < 1200 && window.outerWidth > 780) {
-            sliderSwiper(3, 20);
-        } else if (window.outerWidth <= 780 && window.outerWidth > 760) {
-            sliderSwiper(2.618, 19.78);
-        } else if (window.outerWidth <= 760 && window.outerWidth > 450) {
-            sliderSwiper(2, 10);
-        } else if (window.outerWidth <= 450 && window.outerWidth > 376) {
-            sliderSwiper(1.5, 10);
-        } else if (window.outerWidth <= 376) {
-            sliderSwiper(1.33, 16);
+    navigation: {
+        nextEl: '.control__btn_next',
+        prevEl: '.control__btn_prev',
+    },
+
+    breakpoints: {
+        250: {
+            watchOverflow: false,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            loop: false
+        },
+        350: {
+            watchOverflow: false,
+            slidesPerView: 1.4,
+            spaceBetween: 16,
+            loop: false
+        },
+        460: {
+            watchOverflow: false,
+            slidesPerView: 1.4,
+            spaceBetween: 16,
+            loop: false
+        },
+        540: {
+            watchOverflow: false,
+            slidesPerView: 2,
+            spaceBetween: 10,
+            loop: false
+        },
+
+        760: {
+            slidesPerView: 2.6,
+            spaceBetween: 10,
+            loop: true
+        },
+
+        770: {
+            slidesPerView: 2.618,
+            spaceBetween: 19.78,
+            loop: true
+        },
+
+        1200: {
+            width: 1180,
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop: true
         }
-    };
+        // 540: {
+        //     watchOverflow: false,
+        //     slidesPerView: 1.33,
+        //     spaceBetween: 16,
+        //     loop: false
+        // },
+        // 460: {
+        //     watchOverflow: false,
+        //     slidesPerView: 1.4,
+        //     spaceBetween: 16,
+        //     loop: false
+        // },
+        // 320: {
+        //     watchOverflow: false,
+        //     slidesPerView: 1,
+        //     spaceBetween: 0,
+        //     loop: false
+        // },
+
+    }
 });
-
-function sliderSwiper(slider, spaceBetween) {
-    const swiper = new Swiper('.swiper-container', {
-        slidesPerView: slider,
-        spaceBetween: spaceBetween,
-        loop: true,
-
-        navigation: {
-            nextEl: '.control__btn_next',
-            prevEl: '.control__btn_prev',
-        }
-
-    });
-}
