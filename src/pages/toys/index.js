30eb12db2eb4762ad95e803d422326937dfec73b
dn1984 <kils@list.ru>
2021-06-14 20:42:01 +0400

    Доделан адаптив всех страниц кроме catalog

--- src/pages/toys/index.js
+++ src/pages/toys/index.js
@@ -2,15 +2,10 @@ let input = document.querySelector(".cost__range");
 actualizarInput(input);
 
 document.addEventListener("input", function(evt) {
-    var input = evt.target;
     actualizarInput(input);
 });
 
 function actualizarInput(input) {
-    // var label = input.parentElement.querySelector("label");
-    // label.innerHTML = input.value;
-    var inputMin = input.getAttribute("min");
-    var inputMax = input.getAttribute("max");
-    var unidad = (inputMax - inputMin) / 100;
-    input.style.setProperty("--value", (input.value - inputMin) / unidad);
+    var inputVal = input.value;
+    input.style.setProperty("--value", (inputVal - 0) / 5);
 }
